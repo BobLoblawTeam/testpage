96a0a3b5d1a2a1f20a6edb18994816fe627779c8
hembrew <william.hembree@cdk.com>
2017-07-17 14:22:31 -0700

    Added collapsible with specifications

--- src/App.js
+++ src/App.js
@@ -2,18 +2,23 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import heroImage from './images/batmobile-future-concept-6.jpg';
+import Collapsible from 'react-collapsible';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
         <p className="App-intro">
             <img src={heroImage} className="hero" alt="hero" />
         </p>
+          <Collapsible trigger="Vehicle Specifications">
+              <p>
+                  <ul>
+                      <li>Length: 396 in (33 feet) long</li>
+                      <li>Height: 59.05 in (1.5 m)</li>
+                  </ul>
+              </p>
+          </Collapsible>
       </div>
     );
   }
